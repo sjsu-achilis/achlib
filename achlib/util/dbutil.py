82ad95148276dff32706870dfaab0f3b63a4ea00
Mohapatra <pankaj.mest@gmail.com>
2018-08-02 08:40:15 -0700

    single connection per transaction

--- achlib/util/dbutil.py
+++ achlib/util/dbutil.py
@@ -6,21 +6,34 @@ import psycopg2
 from psycopg2.pool import ThreadedConnectionPool
 
 from achlib.config import file_config
-from achlib.util import logger
+from achlib.util import logger, timit
 
 log = logger.getLogger(__name__)
 config = file_config()
 
+'''
 POOL = ThreadedConnectionPool(1,100, dbname=config.get('DB','dbname'), \
        user=config.get('DB','user'), password=config.get('DB','pswd'), \
        host=config.get('DB','host') , port=config.get('DB','port'))
+'''
 
+def db_connect():
+    try:
+        log.info('connecting to database')
+        conn = psycopg2.connect(dbname=config.get('DB','dbname'), \
+               user=config.get('DB','user'), password=config.get('DB','pswd'), \
+               host=config.get('DB','host') , port=config.get('DB','port'))
+        return conn
+    except:
+        log.exception('cannot connect to db')
 
+@timit
 def db_fetch(query, fetch='all'):
-    conn = POOL.getconn()
+    conn = db_connect()
     cur = conn.cursor()
     try:
         cur.execute("{}".format(query))
+        log.info('querry executed')
         if fetch=='all':
             res = cur.fetchall()
         else:
@@ -28,28 +41,30 @@ def db_fetch(query, fetch='all'):
     except:
         log.error('could not fetch result')
     finally:
-        log.info('putback connection')
-        POOL.putconn(conn)
+        log.info('connection terminated')
+        conn.close()
 
     return res
 
 
+@timit
 def db_insup(query):
-    conn = POOL.getconn()
+    conn = db_connect()
     cur = conn.cursor()
     try:
         cur.execute("{}".format(query))
         conn.commit()
+        log.info('querry executed and commited')
     except:
         log.error('could not execute query')
         return False
     finally:
-        log.info('putback connection')
-        POOL.putconn(conn)
+        log.info('connection terminated')
+        conn.close()
 
     return True
 
-
+'''
 def close_pool():
     try:
         POOL.closeall()
@@ -57,3 +72,4 @@ def close_pool():
     except:
         log.exception('could not close db connection pools')
     return True
+'''
