1c6c1f150fbf2c91d8275e83f61255ecbb57c38a
Mohapatra <pankaj.mest@gmail.com>
2018-08-04 22:52:17 -0700

    local config path

--- achlib/config/__init__.py
+++ achlib/config/__init__.py
@@ -15,5 +15,5 @@ def file_config():
     cfg = SafeConfigParser()
     cfg.readfp(pkg_resources.resource_stream(__name__, "config.ini"))
     if os.path.isfile(LOCAL_CONFIG):
-        cfg.readfp(pkg_resources.resource_stream(__name__, LOCAL_CONFIG))
+        cfg.readfp(open(LOCAL_CONFIG))
     return cfg
